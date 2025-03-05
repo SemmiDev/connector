048de9e9db259c7c4ebc0f41e4ee00d4abeac6cd
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2025-03-05 08:53:25 +0700

    add

--- up.sql
+++ up.sql
@@ -6,33 +6,3 @@ CREATE TABLE IF NOT EXISTS api_tokens (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO api_tokens (name, token, active) VALUES ('glearning', '6qPRWChjOmfziYo0dASFKS+vnkZGxHgg', 1);
-
-drop table if exists sync_logs_batches;
-drop table if exists sync_logs;
-
-CREATE TABLE if not exists sync_logs (
-  id CHAR(26) NOT NULL PRIMARY KEY,
-  id_instansi CHAR(26) DEFAULT NULL,
-  entity ENUM('mahasiswa', 'dosen', 'kelas', 'perkuliahan') NOT NULL,
-  status ENUM('pending', 'processing', 'completed', 'failed') NOT NULL,
-  total_data INT DEFAULT 0,
-  start_time DATETIME NOT NULL,
-  end_time DATETIME,
-  error_message TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  INDEX idx_entity_status (entity, status)
-);
-
-CREATE TABLE if not exists  sync_logs_batches (
-   id CHAR(26) NOT NULL PRIMARY KEY,
-   sync_log_id CHAR(26) NOT NULL,
-   batch_sequence INT NOT NULL,
-   total_data INT DEFAULT 0,
-   success_count INT DEFAULT 0,
-   failed_count INT DEFAULT 0,
-   error_message TEXT,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   FOREIGN KEY (sync_log_id) REFERENCES sync_logs(id) ON DELETE CASCADE
-);
