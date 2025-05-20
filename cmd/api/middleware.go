9e0a69ddb4a331173a16f95ea24f77c7667412a9
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2025-05-20 13:56:04 +0700

    middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/pkg/errors"
-	"gorm.io/gorm"
 	"net/http"
 	"strings"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 // example -> Authorization: Bearer jwtTokenXXX
@@ -13,17 +12,6 @@ const (
 	authorizationHeaderKey = "Authorization"
 	applicationSource      = "glearning"
 )
-
-type ApiToken struct {
-	ID     int64  `json:"id" gorm:"column:id"`
-	Name   string `json:"name" gorm:"column:name"`
-	Token  string `json:"token" gorm:"column:token"`
-	Active uint8  `json:"active" gorm:"column:active"`
-}
-
-func (t *ApiToken) IsActive() bool {
-	return t.Active == 1
-}
 
 func (a *ApplicationServer) WithApiKey() fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
@@ -49,25 +37,23 @@ func (a *ApplicationServer) WithApiKey() fiber.Handler {
 
 		apiKey := fields[1]
 
-		var apiToken ApiToken
-
-		err := a.db.Table("api_key_list").Where("api_key = ?", apiKey).First(&apiToken).Error
-
+		var secret string
+		err := a.db.Table("setting_pt").Where("param = ?", "secret_smartthink").Select("value").Scan(&secret).Error
 		if err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
-					"code":    http.StatusUnauthorized,
-					"status":  "Unauthorized",
-					"success": false,
-					"message": "Api key tidak ditemukan",
-				})
-			}
-
 			return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
 				"code":    http.StatusInternalServerError,
 				"status":  "Internal Server Error",
 				"success": false,
 				"message": err.Error(),
+			})
+		}
+
+		if secret != apiKey {
+			return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"code":    http.StatusUnauthorized,
+				"status":  "Unauthorized",
+				"success": false,
+				"message": "Api key tidak sesuai",
 			})
 		}
 
